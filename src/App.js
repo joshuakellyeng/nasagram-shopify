69ce8f204493b8d05464b5a8a337fa12c50b2ec8
Joshua Kelly <joshuakellyeng@gmail.com>
2022-01-19 11:06:37 -0500

    fixing errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import NasagramHeader from './components/NasagramHeader';
 import ResultsCard from './components/ResultsCard';
